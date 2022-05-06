2e861f412a106cb8b990d3576ac420b90f910edc
wangzhulin <1305986079@qq.com>
2022-05-06 15:58:39 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,40 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+const { searchPlugin } = require('@vuepress/plugin-search')
+
 module.exports = {
   lang: 'zh-CN',
   title: 'interviewGuide',
   description: '面试指南',
-  base: '/interviewGuide/'
+  base: '/interviewGuide/',
+  theme: defaultTheme({
+    // 配置默认主题
+    navbar: [
+      // NavbarItem
+      {
+        text: 'Foo',
+        link: '/foo/',
+      },
+      // NavbarGroup
+      {
+        text: 'Group',
+        children: ['/group/foo.md', '/group/bar.md'],
+      },
+      // 字符串 - 页面文件路径
+      '/bar/README.md',
+    ],
+  }),
+  plugins: [
+    searchPlugin({
+      // 配置项
+      locales: {
+        '/': {
+          placeholder: 'Search',
+        },
+        '/zh/': {
+          placeholder: '搜索',
+        },
+      },
+    }),
+  ],
+
 }
